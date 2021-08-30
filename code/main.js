121b3a0b3e69e2a297a0b716ff6f3ea5fa3b3552
James Dobbe <dobbe2@gmail.com>
2021-08-30 22:54:31 +0000

    bullets firing, having issues with collision between invaders and wall

--- code/main.js
+++ code/main.js
@@ -6,7 +6,10 @@ const MOVE_SPEED = 200;
 let INVADER_SPEED = 100;
 let CURRENT_SPEED = INVADER_SPEED;
 const LEVEL_DOWN = 100;
-const STAR_SPEED = 32;
+const STAR_SPEED = 132;
+
+//layers of the game board
+layer(['obj', 'ui'], 'obj')
 
 // load assets
 loadSprite("space-invader2", "sprites/space-invader2.png");
@@ -21,18 +24,53 @@ loadSound("pew", "sounds/pew.mp3");
 loadSound("splode", "sounds/splode.mp3");
 
 
-
 scene("main", () => {
 
 //add the music
 const music = play('invaders');
-music.loop();
+//loop incase level lasts longer than 45 seconds
+// music.loop();
 
+//background black as space!
   add([
     rect(width(), height()),
     color(0,0,0,1),
   ]);
+//add stars
+  add([
+		sprite("stars"),
+		scale(width() / 580, height() / 580),
+		pos(50, 0),
+		"stars",
+	]);
 
+	add([
+		sprite("stars"),
+		scale(width() / 680, height() / 680),
+		pos(100, 80),
+		"stars",
+	]);
+
+  add([
+		sprite("stars"),
+		scale(width() / 580, height() / 580),
+		pos(150, 120),
+		"stars",
+	]);
+
+	add([
+		sprite("stars"),
+		scale(width() / 680, height() / 680),
+		pos(200, 260),
+		"stars",
+	]);
+
+action("stars", (r) => {
+		r.move(0, STAR_SPEED);
+		if (r.pos.y >= height()) {
+			r.pos.y -= height() * 2;
+		}
+	});
 
   addLevel([
     '!                &',
@@ -59,10 +97,10 @@ music.loop();
   ], {
     width: 30,
     height: 22,
-    '^' : [ sprite('space-invader'), scale(0.5), 'space-invader'],
-    '@' : [ sprite('space-invader2'), scale(0.5), 'space-invader'],
-    '!' : [ sprite('wall'), 'left-wall'],
-    '&' : [ sprite('wall'), 'right-wall']
+    '^' : [ sprite('space-invader'), scale(0.5), 'space-invader', layer("ui")],
+    '@' : [ sprite('space-invader2'), scale(0.5), 'space-invader', layer("ui")],
+    '!' : [ sprite('wall'), layer("ui"), 'left-wall'],
+    '&' : [ sprite('wall'), layer("ui"), 'right-wall']
     
   })
 
@@ -88,6 +126,44 @@ const player = add([
       player.pos.x = width()-20;
     }
   })
+
+  //fire bullet
+  function spawnBullet(p){
+  add([
+    rect(4,12), 
+    pos(p), 
+    origin('center'), 
+    color(rand(rgb(0, 0, 0), rgb(1, 1, 1))), 
+    'bullet'
+    ])
+}
+//fire bullets
+keyPress('space', () => {
+  spawnBullet(player.pos.add(0, -17))
+})
+
+const BULLET_SPEED = 400
+//move bullet
+action('bullet', (b) => {
+  b.move(0, -BULLET_SPEED)
+  if(b.pos.y < 0) {
+    destroy(b);
+  }
+})
+
+  //move invaders
+  action('space-invader', (s) => {
+  s.move(CURRENT_SPEED, 0)
+})
+
+//right wall collision and reverse
+// collides('space-invader', 'right-wall', () => {
+//   CURRENT_SPEED = -INVADER_SPEED
+//   every('space-invader', (s) => {
+//      s.move(0, LEVEL_DOWN);
+//   })
+// })
+
 })
 
 go("main");
